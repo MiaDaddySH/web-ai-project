834af5647485d0219db383ca70ebf5532efca642
Yuangang Sheng <martin_sheng@hotmail.com>
2026-09-19 15:11:07 +0200

    feat: add Aliyun OSS Spring Boot starter and auto-configuration demo

--- tlias-web-management/src/main/java/com/sheng/TliasWebManagementApplication.java
+++ tlias-web-management/src/main/java/com/sheng/TliasWebManagementApplication.java
@@ -2,9 +2,7 @@ package com.sheng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
 
-@ServletComponentScan
 @SpringBootApplication
 public class TliasWebManagementApplication {
 
